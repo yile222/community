22648029803235f5b5e13cdd1988ebb070b7d48d
yile222 <1156002570@qq.com>
2020-04-18 17:49:08 +0800

    community1

---
+++ src/test/java/com/community/community1/Community1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.community1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Community1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
